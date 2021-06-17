a81778e4415aebab17aa177189421f4d355e8885
Sabry Moulana <sabrym@99x.io>
2021-06-17 15:09:42 +0530

    updated

--- sidebars.js
+++ sidebars.js
@@ -2,22 +2,28 @@ module.exports = {
   docs: [
     'how-we-work',
     {
-        type: 'category',
-        label : 'Architecting new products',
-        collapsed:true,
-        items : [ 
-          'our-process/product-architecture',
-          'our-process/continuous-architecture',
-          'our-process/product-health',
-          'our-process/due-diligence'
-        ],
-        rootItem: 'architecting-new-products'
+      type: 'category',
+      label: 'Architecting new products',
+      collapsed: true,
+      items: [
+        'architecting-new-products',
+        {
+          Docs: [
+            'our-process/product-architecture',
+            'our-process/continuous-architecture',
+            'our-process/product-health',
+            'our-process/due-diligence'
+          ]
+        }
+
+      ],
+
     },
     {
       type: 'category',
-      label : 'Focus Areas',
-      collapsed:true,
-      items : [ 
+      label: 'Focus Areas',
+      collapsed: true,
+      items: [
         'focus-areas/web-application',
         'focus-areas/cloud-architecture',
         'focus-areas/devops',
@@ -26,9 +32,9 @@ module.exports = {
     },
     {
       type: 'category',
-      label : 'Case Studies',
-      collapsed:true,
-      items : [ 
+      label: 'Case Studies',
+      collapsed: true,
+      items: [
         'case-studies/design-system',
         'case-studies/microservices',
         'case-studies/microfrontends',
