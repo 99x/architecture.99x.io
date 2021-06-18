f5ca1bbb4aafae76b970a9573570d1da497247b6
gh-actions <actions@github.com>
2021-06-18 08:54:37 +0000

    Deploy website - based on 3e25ffc9527e42a84e5534e1eecedd3362dbd334

--- assets/js/894e5682.84744e7f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkarchitecture_at_99x=self.webpackChunkarchitecture_at_99x||[]).push([[222],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"},{"title":"Hello","permalink":"/blog/hello-world"},{"title":"Hola","permalink":"/blog/hola"}]}')}}]);
---
+++ assets/js/a80da1cf.e5ed9a91.js
@@ -0,0 +1,1 @@
+(self.webpackChunkarchitecture_at_99x=self.webpackChunkarchitecture_at_99x||[]).push([[3205],{7671:function(u){"use strict";u.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/blog/tags/docusaurus"}')}}]);
